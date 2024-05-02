006ab48a921d9e2a7a49c3ac3315526471dbb80d
luqmant51 <luqmant51@gmail.com>
2024-05-02 18:49:27 +0500

    initial

---
+++ devdocsjs/src/app/(protected)/sample/page-1/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Page1 from "../../../../modules/sample/Page1";
+
+const Page = () => {
+    return <Page1/>;
+};
+
+export default Page;
+
---
+++ devdocsjs/src/app/(protected)/sample/page-2/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Page2 from "../../../../modules/sample/Page2";
+
+const Page = () => {
+    return <Page2/>;
+};
+
+export default Page;
+
